8524d204b9b7da254051ba209e592978cd57f4d7
dhivaharjey <dhivahar19399@gmail.com>
2025-03-31 20:28:48 +0530

    online users

--- src/components/Chat/ChatSidebar.jsx
+++ src/components/Chat/ChatSidebar.jsx
@@ -39,7 +39,7 @@ const ChatSidebar = () => {
               <span className="text-sm hidden lg:flex"> Online Users</span>
             </label>
             <span className="text-[8px] text-zinc-500 lg:hidden ">Online</span>
-            <span className="hidden lg:flex">({onlineUsers?.length - 1})</span>
+            <span className="hidden lg:flex">({onlineUsers?.length})</span>
           </div>
         </div>
         <div className="overflow-y-auto w-full py-3">
